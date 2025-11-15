cc0bfb86a868aa72e133893f265e2543e79675ca
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-15 23:21:49 +0200

    Add "No Added Games Yet" option

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -17,14 +17,11 @@ export default function Catalog() {
     return (
         <section id="catalog-page">
             <h1>Catalog</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
             <div className="catalog-container">
+                {games.length === 0 && <h3 className="no-articles">No Added Games Yet</h3>}
 
                 {games.map(game => <GameCard key={game._id} {...game} />)}
-
             </div>
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            {/* <!-- <h3 className="no-articles">No Added Games Yet</h3> --> */}
         </section>
     )
 }
