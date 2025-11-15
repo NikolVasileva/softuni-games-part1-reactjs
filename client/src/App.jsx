2583b196f5c7c8b37098efd23f5211ff5694651f
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-15 19:39:02 +0200

    Create Home Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
-
+      <Home />
       <Footer />
     </>
   )
