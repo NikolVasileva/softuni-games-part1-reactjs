51ad6fda05804d545d79473bef195c13a6efa0c4
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-15 22:44:47 +0200

    Creating GameCard Component
    this component can be used at Catalog and Home Components

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -28,33 +28,6 @@ export default function Catalog() {
                     </div>
                 </div>
 
-                <div className="game">
-                    <img src="./images/elden ring.png" alt="Elden Ring" />
-                    <div className="details-overlay">
-                        <p className="name">Elden Ring</p>
-                        <p className="genre">Action RPG</p>
-                        <a href="#" className="details-button">Details</a>
-                    </div>
-                </div>
-
-                <div className="game">
-                    <img src="./images/minecraft.png" alt="Minecraft" />
-                    <div className="details-overlay">
-                        <p className="name">Minecraft</p>
-                        <p className="genre">Sandbox</p>
-                        <a href="#" className="details-button">Details</a>
-                    </div>
-                </div>
-
-                <div className="game">
-                    <img src="./images/cyberpunk.png" alt="Cyberpunk 2077" />
-                    <div className="details-overlay">
-                        <p className="name">Cyberpunk 2077</p>
-                        <p className="genre">Action RPG</p>
-                        <a href="#" className="details-button">Details</a>
-                    </div>
-                </div>
-
             </div>
             {/* <!-- Display paragraph: If there is no games  --> */}
             {/* <!-- <h3 className="no-articles">No Added Games Yet</h3> --> */}
