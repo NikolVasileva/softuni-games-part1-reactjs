9a860c55b6085dd9483ce16b613744810dcefb30
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-16 16:42:02 +0200

    Create POST Game Method

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,14 +1,46 @@
+import { useNavigate } from "react-router";
+
 export default function GameCreate() {
+
+    const navigate = useNavigate();
+
+    const createGameHandler = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+
+        data.players = Number(data.players);
+        data._createdOn = Date.now();
+
+        try {
+            const response = await fetch("http://localhost:3030/jsonstore/games", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+    
+            const result = await response.json();
+            console.log(result);
+            navigate("/games");
+
+        } catch(err) {
+            alert(err.message);
+        }
+    }
+
     return(
         <section id="add-page">
-        <form id="add-new-game">
+        <form id="add-new-game" onSubmit={createGameHandler}>
             <div className="container">
 
                 <h1>Add New Game</h1>
 
                 <div className="form-group-half">
-                    <label htmlFor="gameName">Game Name:</label>
-                    <input type="text" id="gameName" name="gameName" placeholder="Enter game title..." />
+                    <label htmlFor="title">Game Name:</label>
+                    <input type="text" id="title" name="title" placeholder="Enter game title..." />
                 </div>
 
                 <div className="form-group-half">
@@ -17,13 +49,13 @@ export default function GameCreate() {
                 </div>
 
                 <div className="form-group-half">
-                    <label htmlFor="activePlayers">Active Players:</label>
-                    <input type="number" id="activePlayers" name="activePlayers" min="0" placeholder="0" />
+                    <label htmlFor="players">Active Players:</label>
+                    <input type="number" id="players" name="players" min="0" placeholder="0" />
                 </div>
 
                 <div className="form-group-half">
-                    <label htmlFor="releaseDate">Release Date:</label>
-                    <input type="date" id="releaseDate" name="releaseDate" />
+                    <label htmlFor="date">Release Date:</label>
+                    <input type="date" id="date" name="date" />
                 </div>
 
                 <div className="form-group-full">
